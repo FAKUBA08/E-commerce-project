7e4561f21314803e19316cb52e0ef3e4fc380eb9
FAKUBA08 <badmusfaruq01@gmail.com>
2024-06-11 15:15:46 +0100

    all

--- js/app.js
+++ js/app.js
@@ -1,30 +1,24 @@
-
-const countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
+const countDownDate = new Date("JUN 30, 2024 15:37:25").getTime();
 
 const x = setInterval(function () {
   var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    days +"d :  "     +hours + "h : "   +   minutes + "m : "   + seconds + "s ";
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
 
+// Swiper initialization
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 10,
@@ -48,7 +42,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// Animations
+// ScrollReveal animations
 ScrollReveal().reveal(".top_nav", {
   origin: "bottom",
   distance: "20px",
@@ -60,7 +54,6 @@ ScrollReveal().reveal(".nav", {
   opacity: 0,
   delay: 100,
 });
-
 ScrollReveal().reveal(".header", {
   origin: "bottom",
   distance: "20px",
@@ -82,12 +75,10 @@ ScrollReveal().reveal(".footer", {
   delay: 100,
 });
 
-// mobile nav
+// Mobile nav toggle
 const hamburger = document.querySelector(".hamburger");
 const Nav = document.querySelector(".mobile_nav");
 
 hamburger.addEventListener("click", () => {
   Nav.classList.toggle("mobile_nav_hide");
 });
-
-// 
