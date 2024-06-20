30aee3dd69978199b4f1154c45ab3bd1ebc3e359
FAKUBA08 <badmusfaruq01@gmail.com>
2024-06-20 14:52:07 +0100

    all

--- cart/cart.js
+++ cart/cart.js
@@ -1,4 +1,26 @@
-let check=document.querySelector('.check')
-check.addEventListener('submit',function(event){
-event.preventDefault()
-})
+// Get the full image view
+var fullImageView = document.getElementById("fullImageView");
+
+// Get the image and insert it inside the full image view
+var img = document.getElementById("profileImg");
+var fullImage = document.getElementById("fullImage");
+
+img.onclick = function() {
+  fullImageView.style.display = "block";
+  fullImage.src = this.src;
+}
+
+// Get the <span> element that closes the full image view
+var closeBtn = document.getElementsByClassName("close-btn")[0];
+
+// When the user clicks on <span> (x), close the full image view
+closeBtn.onclick = function() { 
+  fullImageView.style.display = "none";
+}
+
+// Optional: Close the full image view when the user clicks anywhere outside the content
+window.onclick = function(event) {
+  if (event.target == fullImageView) {
+    fullImageView.style.display = "none";
+  }
+}
--- js/app.js
+++ js/app.js
@@ -131,3 +131,24 @@ accountUser.addEventListener('click',function(){
   nav.classList.toggle("details_User");
   // detailsSeen.style.display="block"
 })
+
+var fullImageView = document.getElementById("fullImageView");
+
+var img = document.getElementById("profileImg");
+var fullImage = document.getElementById("fullImage");
+
+img.onclick = function() {
+  fullImageView.style.display = "block";
+  fullImage.src = this.src;
+}
+
+var closeBtn = document.getElementsByClassName("close-btn")[0];
+
+closeBtn.onclick = function() { 
+  fullImageView.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == fullImageView) {
+    fullImageView.style.display = "none";
+  }
+}
