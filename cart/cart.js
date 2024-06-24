4d9d118c9131776578eea355bc5fc2911a61ef88
FAKUBA08 <badmusfaruq01@gmail.com>
2024-06-24 10:11:12 +0100

    all

--- cart/cart.js
+++ cart/cart.js
@@ -1,7 +1,7 @@
-// Get the full image view
+
 var fullImageView = document.getElementById("fullImageView");
 
-// Get the image and insert it inside the full image view
+
 var img = document.getElementById("profileImg");
 var fullImage = document.getElementById("fullImage");
 
@@ -10,15 +10,12 @@ img.onclick = function() {
   fullImage.src = this.src;
 }
 
-// Get the <span> element that closes the full image view
 var closeBtn = document.getElementsByClassName("close-btn")[0];
 
-// When the user clicks on <span> (x), close the full image view
 closeBtn.onclick = function() { 
   fullImageView.style.display = "none";
 }
 
-// Optional: Close the full image view when the user clicks anywhere outside the content
 window.onclick = function(event) {
   if (event.target == fullImageView) {
     fullImageView.style.display = "none";
